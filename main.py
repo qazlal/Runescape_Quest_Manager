b53ca8f65c7ecba58ee2511a2d2176261ba14d77
Qazlal <qazlal.the.shark@gmail.com>
2016-02-04 23:20:05 -0800

    created output display functions
    Quests are printed in columns that can be customized easily; skill
    requirements aren't quite as pretty yet.

--- main.py
+++ main.py
@@ -5,4 +5,15 @@
 # import quest_data as data #so far don't need this
 import quest_tools as tools
 
-print tools.Get_Req(193)
+InputQuest = int('100')
+
+# print ""
+print "This is the quest " + tools.Quest_Name(InputQuest)
+print ""
+
+outputRequirements = tools.Get_Req(InputQuest)
+tools.Display_QuestList(outputRequirements)
+print ""
+tools.Display_SkillList(outputRequirements)
+
+print ""
--- quest_tools.py
+++ quest_tools.py
@@ -1,18 +1,24 @@
 import quest_data as data
+import itertools
+import math
 
 def Get_Req(nQuest,bSRec=True,bQRec=True):
 	#load skills from input quest
-	SReq=data.quests[nQuest][2]
+	SReq=data.quests[nQuest][2] #this is a dictionary
 	if bSRec: SReq = Combine_SReq(SReq,data.quests[nQuest][3])
 
 	#load quest req's from input quest
 	QReq=data.quests[nQuest][4]
 	if bQRec: QReq.extend(data.quests[nQuest][5])
 
+	#QReq_final is a set of all quests required, that this func. will fill
+	QReq_final = set()
+
+	#TO DO: investigate if using sets instead of lists would benefit this function
 	#loop through quest requirements until they're all gone
 	while QReq:
 		for q in QReq:
-			print data.quests[q][1] #name of the quest it's looking at
+			# print data.quests[q][1] #name of the quest it's looking at
 
 			#take the skill req's of the quest and add to the total skill req's
 			SReq = Combine_SReq(SReq,data.quests[q][2])
@@ -22,9 +28,12 @@ def Get_Req(nQuest,bSRec=True,bQRec=True):
 			QReq.extend(data.quests[q][4])
 			if bQRec: QReq.extend(data.quests[q][5])
 
+			#add this quest to a list of all quest requirements
+			QReq_final.add(q)#reminder that this is a set datatype
+
 			#remove this quest from the list now that we're done
 			QReq[:] = (value for value in QReq if value != q) #removes all instances of q from list
-	return SReq
+	return [SReq,QReq_final]
 
 #this is mostly an internal function for Get_Req
 def Combine_SReq(d1,d2):
@@ -40,6 +49,57 @@ def Combine_SReq(d1,d2):
 		else:
 			d3[key]=d2[key]
 	return d3
+
+#get the name of a quest from its index
+def Quest_Name(nQuest):
+	return data.quests[nQuest][1]
+
+#prints a list in a multi-column format
+def Column_PrintList(lRaw,nColumn=4,spacing=25):
+	toZip = []
+
+	#for across then down order
+	# for i in range(nColumn):
+		# toZip.append(lRaw[i::nColumn])
+
+	#for down then across order
+	#ciel rounds up; need to convert to float first though or it'll truncate
+	nRow = int(math.ceil(len(lRaw)/float(nColumn))) #needs to be int for slicing
+	for i in range(nColumn):
+		toZip.append(lRaw[i*nRow:(i+1)*nRow])
+
+	#generate a format string for however many columns
+	#makes something like: '{:<20}{:<20}{:<}'
+	formatString=''
+	for i in range(nColumn-1):
+		formatString = formatString + '{:<'+str(spacing)+'}'
+	formatString = formatString + '{:<}'
+
+	#the asterix "unpacks" the list of lists
+	for abc in itertools.izip_longest(*toZip):
+		abc = [x if x else "" for x in abc] #replaces None with empty string
+		print formatString.format(*abc)
+
+#prints a set datatype of quests in a pretty way
+def Display_QuestList(toPrint):
+	listPrint = list(toPrint[1]) #get a list version of the set of quests
+	listPrint.sort() #sort it numerically
+	#my simple way:
+	# for quest in listPrint:
+	# 	print data.quests[quest][1] #print the name of each quest
+	#adapted from stackechange:
+	listFinal = []
+	for q in listPrint:
+		listFinal.append(data.quests[q][1])
+	print("Quest Requirements:")
+	Column_PrintList(listFinal)
+
+#prints a dictionary datatype of skills requirements in a pretty way
+def Display_SkillList(toPrint):
+	print("Skill Requirements:")
+	for skill in toPrint[0]:
+		#string.title() makes all lowercase besides first letter of words
+		print skill.title() + " " + str(toPrint[0][skill])
 
 #UNTESTED; pseudocode. Quest data not functional at the moment.
 # def lFindNotCompleted(nQuest):
